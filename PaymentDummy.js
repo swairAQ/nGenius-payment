a44928316f222ac1446d23fd770b3316ccdf4e1c
swair.a <swair.a@dplit.com>
2020-12-28 14:43:15 +0500

    Autthorized payment

--- PaymentDummy.js
+++ PaymentDummy.js
@@ -5,43 +5,97 @@ import {
     initiateSamsungPay,
     initiateApplePay,
 } from '@network-international/react-native-ngenius';
+import AsyncStorage from '@react-native-community/async-storage';
+
+//MARK:- Globals
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
+const OutletId = "6f4c7c5f-fba1-40d1-9c8d-fb0dec8d8c9f"
 const appToken = "Basic MDA0ZGNiMTItMmM5Ny00ZmZhLThlYjctYjJjM2FiMWRiNjNlOmRhNzgxNmVlLWFiZjEtNGY0Yi04OTAzLTIzOTdkOWE5ZWM3MA=="
+
 const accessTokenUrl = "https://api-gateway.sandbox.ngenius-payments.com/identity/auth/access-token"
-const orderUrl = "https://api-gateway.sandbox.ngenius-payments.com/transactions/outlets/6f4c7c5f-fba1-40d1-9c8d-fb0dec8d8c9f/orders"
+const orderUrl = "https://api-gateway.sandbox.ngenius-payments.com/transactions/outlets/" + OutletId + "/orders"
+const AuthPayment = "https://api-gateway.sandbox.ngenius-payments.com/transactions/outlets/" + OutletId + "/orders/86b28264-8ae7-4364-937f-99d78b723ea3/payments/d7868d19-aae5-4dcb-9d3c-9b93365d40d4/captures"
+
+//MARK:- Main
 export default function PaymentMethod() {
+
+    //MARK:- Variables
     var flatListref = useRef()
     const [accessToken, setAccessToken] = useState(0)
 
     const [payment, setPayment] = useState(0)
+    const [deductionAmount] = useState(1)
 
     const [order, setOrder] = useState(0)
     const [logs, setlogs] = useState([])
+
     // order is the order response received from NGenius create order API
     const makeCardPayment = async (order) => {
         try {
-
             logs.push("Making card payment...")
             const resp = await initiateCardPayment(order);
 
             Alert.alert("Payment response", resp.status)
-            console.log(resp.status)
 
             logs.push("Card payment " + resp.status)
-
             setlogs(logs)
+
+            deductPayment()
+
         } catch (err) {
 
             logs.push("Payment error " + err.status)
-
             setlogs(logs)
             Alert.alert("Payment error", err.status)
-            console.log({ err });
         }
     };
+    const deductPayment = () => {
+        AsyncStorage.getItem('Resources').then(info => {
+            logs.push("Deducting " + deductionAmount + "AED from total ...")
+            setlogs(logs)
+            const c = info ? JSON.parse(info) : [];
 
+            const AuthPaymentUrl = orderUrl + "/" + c.orderId + "/payments/" + c.paymentId + "/captures"
+            console.log(accessToken + " ==== >access token")
+
+            console.log(AuthPaymentUrl + " ==== >payment url")
+            fetch(AuthPaymentUrl,
+                {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/vnd.ni-payment.v2+json',
+                        'Content-Type': 'application/vnd.ni-payment.v2+json',
+                        'Authorization': "Bearer " + accessToken
+                    },
+                    body: JSON.stringify({
+                        "action": "AUTH",
+                        "amount": { "currencyCode": "AED", "value": deductionAmount }
+                    })
+                })
+                .then(res => res.json())
+                .then(obj => {
+                    if (obj.amount) {
+                        logs.push(deductionAmount + "AED deducted from " + obj.amount.value + "...")
+                        setlogs(logs)
+
+                    }
+                    else {
+                        Alert("Deduction error", "Something went wrong")
+
+                        logs.push("Order error...")
+                        logs.push(obj)
+
+                        setlogs(logs)
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+
+        });
+    }
 
     const getAccessToken = () => {
         logs.push("Getting access token...")
@@ -82,13 +136,19 @@ export default function PaymentMethod() {
                     'Authorization': "Bearer " + token
                 },
                 body: JSON.stringify({
-                    "action": "SALE",
+                    "action": "AUTH",
                     "amount": { "currencyCode": "AED", "value": payment }
                 })
             })
             .then(res => res.json())
             .then(obj => {
+                console.log(JSON.stringify(obj))
                 if (obj.amount) {
+                    const embeded = obj._embedded.payment[0]
+                    const paymentId = embeded._id.split(':')[2]
+
+                    const resource = { "orderId": obj.reference, "paymentId": paymentId }
+                    AsyncStorage.setItem("Resources", JSON.stringify(resource))
                     setOrder(obj)
                     logs.push("Order accessed...")
                     setlogs(logs)
@@ -136,7 +196,7 @@ export default function PaymentMethod() {
             >
                 <Text style={{ color: 'white', fontWeight: '700', fontSize: 17 }}>Place order</Text>
             </TouchableOpacity>
-
+            <Text style={{ fontSize: 7, color: 'gray', marginTop: 5, }}>v1.0.1(2)</Text>
             <FlatList
                 onContentSizeChange={() => flatListref.current.scrollToEnd()}
                 ref={flatListref}
